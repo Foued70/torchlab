98cfd61852e2bb199c2c79f9c6d6ef504aeff90e
Marco Scoffier <marco@lofty.com>
2013-01-02 18:21:16 -0500

    test code for fast ray_plane intersection

--- src/retexture/run.lua
+++ src/retexture/run.lua
@@ -94,12 +94,17 @@ function get_occlusions(pt,dir,obj)
 end
 
 
+
 function fast_ray_face_intersection(pt,dir,normals,d,face_verts)
    -- angle between plane_norms and direction
 
 end
 
--- can cache in the pose and parallelize
+-- FIXME Cache : not dependant on pose position. Can reuse between
+-- multiple poses, by computing once for an image size and scale and
+-- xdeg, ydeg and center.
+--
+-- FIXME Parallelize: ray packets, or simple increment if possible.  
 function compute_dirs(p,i,scale)
    local imgw = p.w[i]
    local imgh = p.h[i]
@@ -115,64 +120,93 @@ function compute_dirs(p,i,scale)
    return dirs
 end
 
-function fast_get_occlusions(p,i,obj,scale)
+function compute_bbox()
+
+end
+
+function build_BVH_tree ()
+   -- 1) all faces part of root node
+   -- 2) pick dimension to split
+   -- 3) intersect faces with 
+end
+
+function fast_ray_plane_intersection(pt,dirs,normals,d)
+   -- # compute distance along ray (pt, in all directions dir) to all face planes
+   -- pow(-1) is robust to 0s. will place inf in cell
+   -- distances is nfaces * ndirs
+   -- angles = 1/normal dot dir
+   local distances = torch.mm(normals,dirs:t()):pow(-1)
+   
+   -- ts is vector of nfaces
+   -- normals dot xyz + plane_d (t's are distance along the ray where
+   -- the plane intersection happens
+
+   local ts     = - (torch.mv(normals,pt) + d)
+
+   -- iterate over directions here FIXME move to ffi
+   for i = 1,distances:size(2) do
+      distances:select(2,i):cmul(ts)
+   end
+
+   return distances
+end
+
+function fast_get_occlusions(p,pi,obj,scale)
    local timer = torch.Timer.new()
-   -- FIXME slice dirs to limit the number of angles computed at a
-   -- time
-   local dirs   = compute_dirs(p,i,scale)
+
+   local dirs  = compute_dirs(p,pi,scale)
    local time0 = timer:time()
    print(string.format("Compute dirs: %2.4fs",time0.real))
-   local pt = p.xyz[i]
-   local dmap = torch.Tensor(dirs:size(1)):fill(-1)
+
+   local pt    = p.xyz[pi] -- select pose xyz
+
+   -- output depth map
+   local dmap  = torch.Tensor(dirs:size(1)):fill(-1)
+
+   local face_verts = obj.face_verts
+
+
+   local distances = fast_ray_plane_intersection(pt,dirs,obj.normals,obj.d)
+
+   -- prep for 
+   -- angles < 0 and at inf can be skipped
+   local dists,index = torch.sort(distances,1)
+
+   -- dists < 0.5 and > 1000m are not to be processed
+   local valid = torch.gt(dists,0.5):cmul(torch.lt(dists,1e3))
+   local vsum  = valid:sum(1)[1]
+   local time1 = timer:time()
+   print(string.format("Multiply and sort: %2.4fs",time1.real-time0.real))
+
    -- precompute the differences between consecutive face verts used
    -- for the line eq. when testing for intersection.
-   local face_verts = obj.face_verts
    local diff_verts = torch.Tensor(face_verts:size())
    local temp_verts = torch.Tensor(face_verts:size(2),face_verts:size(3))
    diff_verts[{{},1,{}}] = face_verts[{{},1,{}}] - face_verts[{{},face_verts:size(2),{}}]
    for i = 2,diff_verts:size(2) do 
       diff_verts[{{},i,{}}] = face_verts[{{},i,{}}] - face_verts[{{},i-1,{}}]
    end
+
+   -- precompute the indexes of the dominant dimensions for each face 
    local most_planar_normals = torch.IntTensor(obj.nfaces,2)
    for i = 1,obj.nfaces do
       local _,ds = torch.sort(torch.abs(obj.normals[i]))
       most_planar_normals[i]:copy(ds:narrow(1,1,2))
    end
-   local time1 = timer:time()
-   print(string.format("Compute planar norms: %2.4fs",time1.real-time0.real))
-   -- pow(-1) is robust to 0s. will place inf in cell
-   -- angles is nfaces * ndirs
-   local angles = torch.mm(obj.normals,dirs:t()):pow(-1)
-   local time2  = timer:time()
-   print(string.format("Compute angles: %2.4fs",time2.real-time1.real))
-   -- ts is vector of nfaces
-   local ts     = - (torch.mv(obj.normals,pt) + obj.d)
-   local time3  = timer:time()
-   print(string.format("Compute distances: %2.4fs",time3.real-time2.real))
+   local time2 = timer:time()
+   print(string.format("Compute planar norms: %2.4fs",time2.real-time1.real))
+
+
+   local time_3_1 = timer:time()
    local acc3_1 = 0
    local acc3_2 = 0
    local acc3_3 = 0
    local acc3_4 = 0
    local acc3_5 = 0
 
-   local time3_0 = timer:time()
-
-   -- iterate over directions here FIXME move to ffi
-   for i = 1,angles:size(2) do
-      angles:select(2,1):cmul(ts)
-   end
-
-   -- angles < 0 and at inf can be skipped
-   local dists,index = torch.sort(angles,1)
-
-   -- dists < 0.5 and > 1000m are not to be processed
-   local valid = torch.gt(dists,0.5):cmul(torch.lt(dists,1e3))
-   local vsum  = valid:sum(1)[1]
-   local time3_1 = timer:time()
-   print(string.format("Multiply and sort: %2.4fs",time3_1.real-time3_0.real))
    local visits = 0
    -- loop over directions
-   for i = 1,angles:size(2) do
+   for i = 1,distances:size(2) do
       if vsum[i] > 0 then
          time3_1 = timer:time()
          local j = 1
@@ -254,16 +288,94 @@ function fast_get_occlusions(p,i,obj,scale)
    print(string.format(" - %2.4fs",acc3_3))
    print(string.format(" - %2.4fs",acc3_4))
    print(string.format(" - %2.4fs",acc3_5))
-   print(string.format("total face intersections: %2.4fs",time4.real-time3.real))
+   print(string.format("total face intersections: %2.4fs",time4.real-time2.real))
    print(string.format("  *** TOTAL ***   : %2.4fs",time4.real))
-   print(string.format("visits per ray: %2.2f",visits/angles:size(2)))
+   print(string.format("visits per ray: %2.2f",visits/distances:size(2)))
    return dmap
 end
-
 
 posedir = paths.dirname(posefile)
 scale    = 4
 invscale = 1/scale
+
+function test_compute_dirs()
+   print("Testing compute directions")
+   local pi = 1
+   local dirs = compute_dirs(poses,pi,scale)
+   local err  = 0
+   sys.tic()
+   local outh = poses.h[pi]*invscale
+   local outw = poses.w[pi]*invscale
+   for h = 1,outh do
+      for w = 1,outw do
+         local pt,dir = util.pose.localxy2globalray(poses,pi,(w-1)*scale,(h-1)*scale)
+         err = err + torch.sum(torch.abs(dir:narrow(1,1,3) - dirs[(h-1)*outw + w]))
+      end
+      print(string.format("-- Errors %2.2f in %2.2fs", err, sys.toc())) 
+   end
+end
+
+-- test_compute_dirs()
+
+function test_ray_plane_intersection()
+   print("Testing ray plane intersections")
+   local pi = 1
+   local dirs = compute_dirs(poses,pi,scale) -- :narrow(1,1,3)
+   local obj = target
+   local err  = 0
+   local norms = obj.normals --:narrow(1,1,4)
+   local d     = obj.d -- :narrow(1,1,4)
+
+   sys.tic()
+
+   local pt = poses.xyz[pi] 
+
+   dots = torch.mm(norms,dirs:t())
+   invdots = torch.pow(dots,-1)
+   ts = - ( torch.mv(norms,pt) + d)
+   
+   dists = torch.Tensor(dots:size()):copy(invdots)
+
+   -- iterate over directions 
+   for i = 1,dists:size(2) do
+      dists:select(2,i):cmul(ts) 
+   end
+
+   -- nfaces x ndirections
+   distances = fast_ray_plane_intersection(pt,dirs,norms,d)
+   nerr = 0
+   for fi = 1,norms:size(1) do
+      for di = 1,dirs:size(1) do
+         dot = torch.dot(norms[fi],dirs[di])
+         -- OK print(string.format("[%d][%d] %2.4f", di, di, dot))
+         if torch.abs(dot - dots[fi][di]) > 1e-8 then
+            print("dot product error")
+         end
+         if torch.abs(1/dot - invdots[fi][di]) > 1e-8 then
+            print("inv dot error")
+         end
+         t = -( torch.dot(norms[fi],pt) + d[fi])
+         if torch.abs(t - ts[fi]) > 1e-8 then
+            print("initial t error")
+         end
+         if torch.abs(t/dot - dists[fi][di]) > 1e-8 then
+            print("final error")
+         end
+         i,t = util.geom.ray_plane_intersection(pt,dirs[di],norms[fi],d[fi])
+         if i then
+            local err = torch.abs(t - distances[fi][di])
+            if err > 1e-8 then
+               nerr = nerr + 1
+               print(string.format("[%d][%d] %2.2f <-> %2.2f err: %2.2f", 
+                                   fi,di,
+                                   t,distances[fi][di],err))
+            end
+         end
+      end
+   end
+   print(string.format("-- Errors: %d in %2.2fs", nerr, sys.toc()))
+end
+-- test_ray_plane_intersection()
 for pi = 1,1 do -- #poses do
    sys.tic()
    dmap = fast_get_occlusions(poses,pi,target,scale)
@@ -282,5 +394,6 @@ for pi = 1,1 do -- #poses do
       end
    end
    print("Processed pose "..pi.." in "..sys.toc().." secs")
-   image.display(ddata)
+   dmap:resize(ddata:size())
+   image.display{image={ddata,dmap}}
 end
