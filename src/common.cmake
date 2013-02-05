8aa007d8911da444e01504036bdf6bd6ec62790b
Dustin Byrne <dustin@givmo.com>
2013-02-05 10:55:36 -0500

    cook build tool

--- src/common.cmake
+++ src/common.cmake
@@ -1,14 +1,14 @@
 find_package(Torch REQUIRED)
 
-set(project_dir ${CMAKE_CURRENT_LIST_DIR})
+get_filename_component(project_dir ${CMAKE_CURRENT_LIST_DIR} PATH)
 set(install_root ${project_dir}/build/usr/local)
 
 if (DEFINED ENV{CLOUDLAB_INSTALL_ROOT})
   get_filename_component(install_root "$ENV{CLOUDLAB_INSTALL_ROOT}" ABSOLUTE)
-  message(${install_root})
 endif (DEFINED ENV{CLOUDLAB_INSTALL_ROOT})
 
 
+set(CMAKE_INSTALL_PREFIX ${install_root})
 set(CMAKE_CXX_COMPILER ${install_root}/bin/g++)
 set(CMAKE_CXX_FLAGS "-std=gnu++11")
 
