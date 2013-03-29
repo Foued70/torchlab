6652079ee5445c121e48a952484ed0b923e3d704
Dustin Byrne <dustin@givmo.com>
2013-03-29 10:24:23 -0400

    new repl and Class reloading

--- src/cloudlab-env.lua
+++ src/cloudlab-env.lua
@@ -7,3 +7,9 @@ p = xlua.print
 log = require 'util.log'
 
 CLOUDLAB_SRC = paths.dirname(debug.getinfo(1).short_src)
+
+require 'util.Class'
+
+local shell = require('shell.shell')
+_G.shell_evaluate = shell.evaluate
+_G.shell_is_complete = shell.is_complete
