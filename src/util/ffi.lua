9437ae609c251c374c3092e0cc397ab01bf9dba1
Marco Scoffier <marco@floored.com>
2013-04-17 21:58:41 -0400

    more helper functions to clean up ffi

--- src/util/ffi.lua
+++ src/util/ffi.lua
@@ -1,12 +1,10 @@
 setfenv(1, setmetatable({}, {__index = _G}))
 
-
 -- local 
 ffi = require 'ffi'
 
-
 -- part of ffi for sending tensors to our own C funcs.
-header_template = 
+local header_template = 
 [[
 typedef struct THStorage 
 {
@@ -30,60 +28,73 @@ typedef struct THTensor
 
  ]]
 
-function generate_header_types(template,types)
-   if not template then
-      template = header_template
-   end
-   if not types then
-      types = {
-         THDouble = "double", 
-         THFloat  = "float",
-         THByte  = "unsigned char",
-         THChar  = "char",
-         THShort = "short",
-         THInt   = "int", 
-         THLong  = "long"
-      }
-   end
-   local ret = ""
-   for T,t in pairs(types) do 
-      ret = ret .. template:gsub("TH",T):gsub("real",t)
-   end
-   return ret
+local types = {
+   THDouble = "double", 
+   THFloat  = "float",
+   THByte  = "unsigned char",
+   THChar  = "char",
+   THShort = "short",
+   THInt   = "int", 
+   THLong  = "long"
+}
+
+-- save memory by building table
+header = {}
+for T,t in pairs(types) do 
+   local s = header_template:gsub("TH",T):gsub("real",t)
+   table.insert(header, s)
 end
+-- convert table into string
+header = table.concat(header,'\n')
 
-function generate_header_float_types(template)
-   local types = {
-      THDouble = "double", 
-      THFloat  = "float"
-   }
-   return generate_header_types(template,types)
-end
+-- Load header
+ffi.cdef(header)
 
-function generate_header_int_types(template)
-   local types = {
-      THByte  = "unsigned char",
-      THChar  = "char",
-      THShort = "short",
-      THInt   = "int", 
-      THLong  = "long"
-   }
-   return generate_header_types(template,types)
-end
+-- Method to return pointer to raw data
+function torch.data(obj)
+   -- first cast pointer into the right type
+   local type_obj     = torch.typename(obj)
+   local type_tensor  = type_obj:gfind('torch%.(.*)Tensor')()
+   local type_storage = type_obj:gfind('torch%.(.*)Storage')()
+   if type_tensor then
+      -- return raw pointer to data
+      return ffi.cast(type_tensor .. 'Tensor*', torch.pointer(obj)).storage.data
+   elseif type_storage then
+      -- return raw pointer to data
+      return ffi.cast(type_storage .. 'Storage*', torch.pointer(obj)).data
+   else
+      print('Unknown data type: ' .. type_obj)
+   end
+end 
 
+-- Method to return c pointer to tensor or storage struct to pass to C
+function torch.cdata(obj)
+   -- first cast pointer into the right type
+   local type_obj     = torch.typename(obj)
+   local type_tensor  = type_obj:gfind('torch%.(.*)Tensor')()
+   local type_storage = type_obj:gfind('torch%.(.*)Storage')()
+   if type_tensor then
+      -- return raw pointer to data
+      return ffi.cast('TH' .. type_tensor .. 'Tensor*', torch.pointer(obj))
+   elseif type_storage then
+      -- return raw pointer to data
+      return ffi.cast('TH' .. type_storage .. 'Storage*', torch.pointer(obj))
+   else
+      print('Unknown data type: ' .. type_obj)
+   end
+end 
 
+-- allows us to easily write functions for all tensor types
 function generate_function_types(template,types)
    if not types then 
       types = {'Double', 'Float', 'Long', 'Int', 'Short', 'Char', 'Byte'}
    end
-   local ret = ""
+   local ret = {}
    for _,t in pairs(types) do 
-      ret = ret .. 
-         template:gsub("THTensor_%((%w+)%)",
-                       "TH"..t.."Tensor_%1"):gsub("THTensor",
-                                                  "TH"..t.."Tensor") .. "\n"
+      local s = template:gsub("THTensor", "TH"..t.."Tensor")
+      table.insert(ret, s)
    end
-   return ret
+   return table.concat(ret,'\n')
 end
 
 function generate_function_int_types(template)
@@ -92,6 +103,11 @@ end
 
 function generate_function_float_types(template)
    return generate_function_types(template,{'Double', 'Float'})
+end
+
+function lib_path(libname)
+   local ext = ffi.os == "OSX" and ".dylib" or ".so"
+   return paths.install_lib .. "/torch/lua/lib" .. libname..ext
 end
 
 return (getfenv())
--- src/util/util.lua
+++ src/util/util.lua
@@ -1,34 +1,30 @@
 Class()
 
--- Below is a test for loading C backends using ffi 
-local ffi = require 'ffi'
-
+-- Below is a test for loading C backends using ffi
 local ffi_utils = require 'util.ffi'
 
-local function_templates = 
-[[
-    void THTensor_rotate_by_quat(THTensor *result, 
-                                 THTensor *vectors, 
-                                 THTensor *quat);
- ]]
+local ffi = ffi_utils.ffi
 
--- FIXME make this loading cleaner for other ffi projects
--- ffi doesn't look in the right place by default
-local utillib = paths.install_lib .. "/torch/lua/" .. "libutil.dylib"
 
-if paths.filep(utillib) then 
-   -- load low level c functions
-   ffi.cdef(ffi_utils.generate_header_float_types() ..
-            ffi_utils.generate_function_float_types(function_templates))
 
-   -- don't want to call C functions directly
-   local C  = ffi.load(utillib)
+local utillib = ffi_utils.lib_path("util")
 
-   function rotate_by_quatC (out, vec, q)
-      C.THDoubleTensor_rotate_by_quat(ffi.cast("THDoubleTensor*",torch.pointer(out)),
-                                      ffi.cast("THDoubleTensor*",torch.pointer(vec)),
-                                      ffi.cast("THDoubleTensor*",torch.pointer(q)))
-   end
+if paths.filep(utillib) then
+   local function_templates =
+      [[
+          void THTensor_rotate_by_quat(THTensor *result,
+                                       THTensor *vectors,
+                                       THTensor *quat);
+       ]]
+      -- load low level c functions
+      ffi.cdef(ffi_utils.generate_function_float_types(function_templates))
+
+      -- don't want to call C functions directly
+      local C  = ffi.load(utillib)
+
+      function rotate_by_quatC (out, vec, q)
+         C.THDoubleTensor_rotate_by_quat(torch.cdata(out), torch.cdata(vec), torch.cdata(q))
+      end
 end
 
 -- end of loading C backend test
@@ -38,7 +34,7 @@ end
 -- data in overlaps.  Start with a less general version which takes
 -- h,w,dims matrix as input and outputs r,c,s1*s2,dims as
 -- output. (Contiguous unfold useful for bilateral filtering also).
-function grid_contiguous (m,s1,s2)
+function unfold_contiguous (m,s1,s2)
    local uf = m:unfold(1,s1,s1):unfold(2,s2,s2)
    local out = torch.Tensor(uf:size(1),uf:size(2),uf:size(4)*uf:size(5),uf:size(3))
    for r = 1,uf:size(1) do
@@ -51,7 +47,7 @@ end
 
 
 -- <vals> sorted list of values
--- 
+--
 -- <val> value for which we want pointer into list where everything
 --       before pointer is < val and after >= val
 
