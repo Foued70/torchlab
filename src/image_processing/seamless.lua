52980b30f330b04f3d9cccbd15badcb398ce9a0e
Marco Scoffier <marco@floored.com>
2013-05-30 14:25:57 -0400

    rough gui for seamless texture

---
+++ src/image_processing/seamless.lua
@@ -0,0 +1,218 @@
+require 'image'
+require 'nn'
+
+require 'qtuiloader'
+require 'qtwidget'
+require 'qt'
+
+--load image
+orig = image.load('textures-to-seamless/DSC01096.jpg')
+orig_size = orig:size()
+print(orig_size)
+-- set scale
+scale = 1/4
+scale_size    = torch.LongStorage(orig_size:totable())
+scale_size[2] = scale_size[2]*scale
+scale_size[3] = scale_size[3]*scale
+print(scale_size)
+
+-- set tile
+tile = {}
+
+-- set normalization kernel
+normscale = 0.25
+
+
+-- photoshop style offset
+function offset (imgtile, ox, oy, output)
+   output = output or torch.Tensor()
+   output:resize(imgtile:size())
+
+   output[{{},{1, oy},{1, ox}}]     =
+      imgtile[{{},{-oy,-1},{-ox,-1}}]
+
+   output[{{},{1, oy},{ox+1,-1}}]   =
+      imgtile[{{},{-oy,-1},{1,-ox-1}}]
+
+   output[{{},{oy+1,-1},{1, ox}}]   =
+      imgtile[{{},{1,-oy-1},{-ox,-1}}]
+
+   output[{{},{oy+1,-1},{ox+1,-1}}] =
+      imgtile[{{},{1,-oy-1},{1,-ox-1}}]
+
+   return output
+end
+
+function four_way (imgtile, output)
+
+   output = output or torch.Tensor()
+
+   local h = imgtile:size(2)
+   local w = imgtile:size(3)
+   output:resize(imgtile:size(1),2*h,2*w)
+
+   -- mode mirror
+   output[{{},  {1, h},  {1, w}}] = imgtile
+   output[{{},  {1, h},{w+1,-1}}] = imgtile
+   output[{{},{h+1,-1},  {1, w}}] = imgtile
+   output[{{},{h+1,-1},{w+1,-1}}] = imgtile
+
+   return output
+end
+
+-- kaliedoscope (works when the textures can be mirrored
+function mirror_4way (imgtile, output)
+
+   output = output or torch.Tensor()
+
+   local h = imgtile:size(2)
+   local w = imgtile:size(3)
+   output:resize(imgtile:size(1),2*h,2*w)
+
+   -- mode mirror
+   local imgtileh   = image.hflip(imgtile)
+   local imgtilehv  = image.vflip(imgtileh)
+   local imgtilehvh = image.hflip(imgtilehv)
+   output[{{},  {1, h},  {1, w}}] = imgtilehvh
+   output[{{},  {1, h},{w+1,-1}}] = imgtilehv
+   output[{{},{h+1,-1},  {1, w}}] = imgtile
+   output[{{},{h+1,-1},{w+1,-1}}] = imgtileh
+
+   return output
+end
+
+-- kaliedoscope (works when the textures can be mirrored
+function mirror_2way_h (imgtile, output)
+
+   output = output or torch.Tensor()
+
+   local h = imgtile:size(2)
+   local w = imgtile:size(3)
+   output:resize(imgtile:size(1),2*h,2*w)
+
+   -- mode mirror
+   local imgtileh   = image.hflip(imgtile)
+   output[{{},  {1, h},  {1, w}}] = imgtile
+   output[{{},  {1, h},{w+1,-1}}] = imgtileh
+   output[{{},{h+1,-1},  {1, w}}] = imgtile
+   output[{{},{h+1,-1},{w+1,-1}}] = imgtileh
+
+   return output
+end
+
+-- kaliedoscope (works when the textures can be mirrored
+function mirror_2way_v (imgtile, output)
+
+   output = output or torch.Tensor()
+
+   local h = imgtile:size(2)
+   local w = imgtile:size(3)
+   output:resize(imgtile:size(1),2*h,2*w)
+
+   -- mode mirror
+   local imgtilev   = image.vflip(imgtile)
+   output[{{},  {1, h},  {1, w}}] = imgtile
+   output[{{},  {1, h},{w+1,-1}}] = imgtile
+   output[{{},{h+1,-1},  {1, w}}] = imgtilev
+   output[{{},{h+1,-1},{w+1,-1}}] = imgtilev
+
+   return output
+end
+
+function process()
+
+
+   if not scaled or (scale_size ~= scaled:size()) then
+      scaled = image.scale(orig,scale_size[3],scale_size[2])
+      imyuv  = image.rgb2yuv(scaled)
+      donorm = true
+   end
+
+   tile.y = math.floor(1 + (widget.Yin.value * scale_size[2] / 100))
+   tile.h = math.floor(1 + (widget.Yout.value * scale_size[2] / 100))
+
+   tile.x = math.floor(1 + (widget.Xin.value * scale_size[3] / 100))
+   tile.w = math.floor(1 + (widget.Xout.value * scale_size[3] / 100))
+
+   -- local constrast normalisation
+   if donorm or not nbhd or not ylcn or nbhd:size(1) ~= gs then
+      gs = 9 -- math.floor((tile.w - tile.x) * normscale )
+      -- gaussian kernel must be odd
+      if gs %2 == 0 then gs = gs + 1 end
+
+      nbhd = image.gaussian({size=gs,sigma=1.591/gs,normalize=true})
+      ylcn = image.lcn(imyuv[1],nbhd)
+      nh = ylcn:size(1)
+      nw = ylcn:size(2)
+
+      imgnrml = imyuv:narrow(2,imyuv:size(2)/2 - nh/2,nh):narrow(3,imyuv:size(3)/2 - nw/2,nw)
+      imgnrml[1] = ylcn
+
+      -- for display
+      imgnrml = scaled -- image.yuv2rgb(imgnrml)
+      
+      outh = imgnrml:size(2)
+      outw = imgnrml:size(3)
+
+      yin  = math.max(1,math.min(tile.y, outh - 1))
+      yout = math.max(yin + 1, math.min(tile.h,outh))
+
+      xin  = math.max(1,math.min(tile.x, outw -1))
+      xout = math.max(xin + 1, math.min(tile.w,outw))
+
+      imgtile = imgnrml[{{},{yin,yout},{xin,xout}}]
+      mtile = mirror_2way_v(imgtile,mtile)
+      -- mtile = mirror_4way(imgtile,mtile)
+      -- mtile = offset(imgtile,(yout-yin)/2, (xout-xin)/2,mtile)
+      -- mtile = four_way(imgtile,mtile)
+
+      tile.y = yin
+      tile.h = yout
+      tile.x = xin
+      tile.w = xout
+
+   end
+end
+
+-- display function
+function display()
+
+   win:gbegin()
+   win:showpage()
+
+   -- (1) display input image
+   -- image.display{image=scaled, win=win}
+   image.display{image=mtile, win=win} -- , x = scaled:size(3) + 1}
+
+   -- (2) overlay bounding boxes tile chosen
+   win:setcolor(1,0,0)
+   win:rectangle(tile.x, tile.y, tile.w -tile.x, tile.h - tile.y)
+   win:stroke()
+   win:setfont(qt.QFont{serif=false,italic=false,size=16})
+   win:moveto(tile.x, tile.y-1)
+   win:show('input')
+
+   win:gend()
+end
+
+-- setup GUI (external UI file)
+if not win or not widget then
+   widget = qtuiloader.load('seamless_g.ui')
+   win = qt.QtLuaPainter(widget.frame)
+end
+
+-- setup gui
+timer = qt.QTimer()
+timer.interval = 1
+timer.singleShot = true
+qt.connect(timer,
+           'timeout()',
+           function()
+              process()
+              display()
+              timer:start()
+           end)
+
+widget.windowTitle = 'Seamless texture maker'
+widget:show()
+timer:start()
