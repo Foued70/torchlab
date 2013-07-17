3f0c53857b4c8924195ce9d01a2bec6d4290d55f
Sonia <sonia@floored.com>
2013-07-17 16:42:57 -0400

    fixed hough

--- src/hough/generic/hough.c
+++ src/hough/generic/hough.c
@@ -16,120 +16,6 @@
 #include <luaT.h>
 #include <TH.h>
 
-/***
-    double get_window_average(double *hT, int r, int c, int winr, int winc, int numR, int numC){
-
-    double ret = 0.0;
-    double cnt = 0.0;
-
-    double *tmpPt;
-
-    for (int i = r-winr; i < r+winr; i++){
-    for (int j = c-winc; j < c+winc; j++){
-    int ir = fmin(fmax(0, i), numR-1);
-    int jc = j % numC;
-    tmpPt = hT + (ir * numC) + jc;
-    ret += *tmpPt;
-    cnt ++;
-    }
-    }
-    ret = ret/cnt;
-    return ret;
-    }
-
-    void local_contrast_normalization(double *hT, int numRadius, int numAngles, int filtsize)
-    {
-    double hTOrigArr[numRadius][numAngles];
-    double *hTOrig = &hTOrigArr[0][0];
-
-    memcpy(hTOrig, hT, numRadius * numAngles * sizeof(double));
-    double *tmpHTNPt;
-    double *tmpHTOPt;
-
-    for (int nA = 0; nA < numAngles; nA ++){
-    for (int nR = 0; nR < numRadius; nR ++){
-    tmpHTNPt = hT + (nR * numAngles) + nA;
-    tmpHTOPt = hTOrig + (nR * numAngles) + nA;
-    double avg;
-    if (numAngles % 8 == 0 && nA % (int)(numAngles/8) == 0){
-    avg = get_window_average(hTOrig, nR, nA, 1, 1, numRadius, numAngles);
-    *tmpHTNPt = avg;
-    *tmpHTOPt = avg;
-    }
-    if(*tmpHTNPt > 0){
-    avg = get_window_average(hTOrig, nR, nA, filtsize, filtsize, numRadius, numAngles);
-    if (*tmpHTNPt <= avg) {
-    *tmpHTNPt = 0;
-    }
-    else {
-    *tmpHTNPt = *tmpHTNPt - avg;
-    }
-    }
-    }
-    }
-
-    }
-
-
-    void hough(THDoubleTensor *result, THDoubleTensor *image)
-    {
-    double *res = THDoubleTensor_data(result);
-    double *img = THDoubleTensor_data(image);
-
-    long numRows = (long)(image->size[1]);
-    long numCols = (long)(image->size[2]);
-
-    long numRadius = (long)(result->size[1]);
-    long numAngles = (long)(result->size[2]);
-
-    double maxRadius = sqrt(numRows * numRows + numCols * numCols) * 0.5;
-
-    double incAngles = 2 * M_PI / numAngles;
-    double incRadius = maxRadius / (numRadius - 1);
-
-    double maxImageVal = THDoubleTensor_maxall(image);
-
-    for (int row = 0; row < numRows; row ++)
-    {
-    for (int col = 0; col < numCols; col ++)
-    {
-    //val = img->row, col
-    double *tmpImgPt = img + (row * numCols) + col;
-    double val = *tmpImgPt;
-
-    if (val > 0.05 * maxImageVal)
-    {
-    //increment vote
-    for (int nA = 0; nA < numAngles; nA ++)
-    {
-    double angle = nA * incAngles;
-    double radiu = sin(angle) * (row-numRows/2) + cos(angle) * (col-numCols/2);
-    double nRR = radiu/incRadius;
-    int nR = (int)(nRR);
-
-    if (nRR >=0 && nRR < numRadius)
-    {
-    double *tmpHoughPt = res + (nR * numAngles) + nA;
-    *tmpHoughPt += val;
-
-    }
-    }
-    }
-    }
-    }
-
-    int filtsize = 5;
-    local_contrast_normalization(res, numRadius, numAngles, filtsize);
-    }
-
-    static int libhough_(Main_houghTransform)(lua_State *L){
-    THDoubleTensor *result = luaT_checkudata(L, 1, torch_Tensor);
-    THDoubleTensor *image  = luaT_checkudata(L, 2, torch_Tensor);
-
-    hough(result, image);
-    }
-***/
-
 static int libhough_(Main_houghTransform)(lua_State *L){
   THTensor *result = luaT_checkudata(L, 1, torch_Tensor);
   THTensor *image  = luaT_checkudata(L, 2, torch_Tensor);
@@ -149,6 +35,8 @@ static int libhough_(Main_houghTransform)(lua_State *L){
   real incRadius = maxRadius / (numRadius - 1);
 
   real maxImageVal = THTensor_(maxall)(image);
+  real considerVal = 0.01 * maxImageVal;
+  real ptExistWght = 0.10 * maxImageVal;
 
   long row, col, nA ;
   for (row = 0; row < numRows; row ++)
@@ -157,7 +45,7 @@ static int libhough_(Main_houghTransform)(lua_State *L){
         {
           //val = img->row, col
 
-          if (*img > 0.05 * maxImageVal)
+          if (*img > considerVal)
             {
               //increment vote
               for (nA = 0; nA < numAngles; nA ++)
@@ -170,7 +58,63 @@ static int libhough_(Main_houghTransform)(lua_State *L){
                   if (nRR >=0 && nRR < numRadius)
                   {
                     real *tmpHoughPt = res + (nR * numAngles) + nA;
-                    *tmpHoughPt += *img;
+                    *tmpHoughPt += *img + ptExistWght;
+
+                  }
+                }
+            }
+
+          img++;
+        }
+    }
+
+  return 1;
+}
+
+static int libhough_(Main_houghTransformVertical)(lua_State *L){
+  THTensor *result = luaT_checkudata(L, 1, torch_Tensor);
+  THTensor *image  = luaT_checkudata(L, 2, torch_Tensor);
+
+  real *res = THTensor_(data)(result);
+  real *img = THTensor_(data)(image);
+
+  long numRows = (long)(image->size[0]);
+  long numCols = (long)(image->size[1]);
+
+  long numRadius = (long)(result->size[0]);
+  long numAngles = (long)(result->size[1]);
+
+  real maxRadius = numCols;
+
+  real incAngles = 2 * M_PI / numAngles;
+  real incRadius = maxRadius / (numRadius - 1);
+
+  real maxImageVal = THTensor_(maxall)(image);
+  real considerVal = 0.01 * maxImageVal;
+  real ptExistWght = 0.10 * maxImageVal;
+
+  long row, col, nA ;
+  for (row = 0; row < numRows; row ++)
+    {
+      for (col = 0; col < numCols; col ++)
+        {
+          //val = img->row, col
+
+          if (*img > considerVal)
+            {
+              //increment vote
+              for (nA = 0; nA < numAngles; nA ++)
+                {
+                  //real angle = nA * incAngles;
+                  real angle = 0;
+                  real radiu = col;
+                  real nRR = radiu/incRadius;
+                  long nR = (long)(nRR);
+
+                  if (nRR >=0 && nRR < numRadius)
+                  {
+                    real *tmpHoughPt = res + (nR * numAngles) + nA;
+                    *tmpHoughPt += *img + ptExistWght;
 
                   }
                 }
@@ -194,7 +138,7 @@ static int libhough_(Main_hough_local_contrast_normalization)(lua_State *L){
   long numRadius = (long)(result->size[0]);
   long numAngles = (long)(result->size[1]);
 
-  int filtsize = 5;
+  int filtsize = (numRadius+numAngles)/500 + 1;
 
   real *tmpHTNPt = res;
   real *tmpHTOPt = cln;
@@ -202,8 +146,6 @@ static int libhough_(Main_hough_local_contrast_normalization)(lua_State *L){
   long nR, nA, i, j;
   for (nR = 0; nR < numRadius; nR ++){
     for (nA = 0; nA < numAngles; nA ++){
-      //tmpHTNPt = res + (nR * numAngles) + nA;
-      //tmpHTOPt = cln + (nR * numAngles) + nA;
 
       real avg;
 
@@ -283,13 +225,14 @@ static int libhough_(Main_hough_local_contrast_normalization)(lua_State *L){
 static const struct luaL_reg libhough_(Main__) [] =
 {
   {"houghTransform",              libhough_(Main_houghTransform)},
+  {"houghTransformVertical",       libhough_(Main_houghTransformVertical)},
   {"localContrastNormalization",  libhough_(Main_hough_local_contrast_normalization)},
   {NULL,NULL}
 };
 
 static void libhough_(Main_init) (lua_State *L)
 {
-  //luaT_pushmetatable(L, torch_Tensor);
+  luaT_pushmetatable(L, torch_Tensor);
   luaT_registeratname(L, libhough_(Main__), "libhough");
   lua_pop(L,1);
 }
