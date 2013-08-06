61dd1615f887b4589ea2a713cff3be59179f8f46
Sonia <sonia@floored.com>
2013-08-06 11:56:00 -0400

    binary load and write

--- src/PointCloud/PointCloud.lua
+++ src/PointCloud/PointCloud.lua
@@ -8,10 +8,10 @@ local d2r = pi/180
 
 local PointCloud = Class()
 
-local PC_FILE_EXTENSION = '.xyz'
-local PC_BINARY_EXTENSION = 'bin'
+local PC_ASCII_EXTENSION = '.xyz'
+local PC_BINARY_EXTENSION = '.xyb'
 
-function PointCloud:__init(pcfilename, radius, numstd)
+function PointCloud:__init(pcfilename, radius, numstd, option)
   self.index = nil;
   self.height = 0;
   self.width = 0;
@@ -30,7 +30,14 @@ function PointCloud:__init(pcfilename, radius, numstd)
   
   if pcfilename then
     if util.fs.is_file(pcfilename) then
-      self:set_pc_file(pcfilename, radius, numstd);
+    	if util.fs.extname(pcfilename)==PC_ASCII_EXTENSION then
+	    	self:set_pc_ascii_file(pcfilename, radius, numstd, option)
+	    	--self:set_pc_file(pcfilename, radius, numstd, option)
+	    elseif util.fs.extname(pcfilename)==PC_BINARY_EXTENSION then
+	    	self:set_pc_binary_file(pcfilename)
+	    else
+	    	error('arg #1 must either be empty or a valid file')
+	    end	
     else
       error('arg #1 must either be empty or a valid file')
     end
@@ -44,12 +51,262 @@ function PointCloud:reset_point_stats()
     local d1 = (self.maxval-self.centroid)[1]
     local d2 = (self.centroid-self.minval)[1]
     self.radius=torch.Tensor({math.max(d1[1],d2[1]),math.max(d1[2],d2[2]),math.max(d1[3],d2[3])})
+    
+end
+
+local function hex_to_integer(string_x)
+	
+	if string.len(string_x) > 8 then
+		local neg1 = string.format('%X',-1)
+		local n1 = tonumber(string.sub(neg1,string.len(neg1)-8,string.len(neg1)),16)
+		local n2 = tonumber(string.sub(string_x,string.len(string_x)-8,string.len(string_x)),16)
+		return n2-n1-1
+	else
+		return tonumber(string_x,16)
+	end
+end
+
+function PointCloud:set_pc_binary_file(pcfilename)
+
+	local count = 0;
+    self.height = 0;
+	self.width = 0;
+	local xyz_table={}
+	local rgb_table={}
+    file = io.open(pcfilename,'r')
+	self.format = 0
+	
+    local data = file:read('*all')
+    file:close()
+    
+    data=data..'\n'
+    data=string.gsub(data, '\r', '\n')
+    data=string.gsub(data, '\n+', '\n')
+    
+    local i = -1
+    local minx, miny, minz
+    for x,y,z,r,g,b in string.gmatch(data, '(%x+) (%x+) (%x+) (%x+) (%x+) (%x+)\n') do
+    	
+    	x = hex_to_integer(x)
+    	y = hex_to_integer(y)
+    	z = hex_to_integer(z)
+    	r = hex_to_integer(r)
+    	g = hex_to_integer(g)
+    	b = hex_to_integer(b)
+    	
+    	if i < 0 then
+    		minx = x
+			miny = y
+			minz = z
+			
+			print(torch.Tensor({x/1000,y/1000,z/1000}))
+    	else
+    		x = (x+minx)/1000.0
+    		y = (y+miny)/1000.0
+    		z = (z+minz)/1000.0
+    				
+    		count = count + 1
+			table.insert(xyz_table,{x,y,z})
+			table.insert(rgb_table,{r,g,b})
+    	end
+    	
+    	i=i+1
+	end
+    
+	self.points = torch.Tensor(xyz_table)
+	self.rgb = torch.Tensor(rgb_table)
+	self.count = count
+	
+	self:reset_point_stats()
+	
+	collectgarbage()
+		
+end
+
+function PointCloud:set_pc_ascii_file(pcfilename, radius, numstd)
+    	
+	local count = 0;
+    self.height = 0;
+	self.width = 0;
+    local meanx = 0
+	local meany = 0
+	local meanz = 0
+	local hw_table={}
+	local xyz_table={}
+	local rgb_table={}
+	-- assume z is flatter
+	local rad2d = math.sqrt(math.pow(radius,2)*2/2.25)
+	
+	file = io.open(pcfilename,'r')
+	self.format = 1
+	
+    local data = file:read('*all')
+    file:close()
+    
+    data=data..'\n'
+    data=string.gsub(data, '\r', '\n')
+    data=string.gsub(data, '\n+', '\n')
+    
+    local minx, miny, minz
+    
+    for w,h,x,y,z,r,g,b in string.gmatch(data, '(%d+) (%d+) (%-*%d+%p*%d*) (%-*%d+%p*%d*) (%-*%d+%p*%d*) (%d+) (%d+) (%d+)\n') do 
+    
+    	w = tonumber(w)
+    	h = tonumber(h)
+    	x = tonumber(x)
+    	y = tonumber(y)
+    	z = tonumber(z)
+    	r = tonumber(r)
+    	g = tonumber(g)
+    	b = tonumber(b)
+    	
+    	h=h+1
+		w=w+1
+		if math.sqrt(math.pow(x,2)+math.pow(y,2)) < rad2d then
+	  		count = count + 1
+      	  	meanz = meanz + z
+			table.insert(xyz_table,{x,y,z})
+			table.insert(rgb_table,{r,g,b})
+    	  	if h > self.height then
+	        	self.height = h;
+		    end
+		 	if w > self.width then
+        		self.width = w;
+	  		end
+      		table.insert(hw_table,{h,w,count})
+      	end
+    end
+    
+    if count == 0 then -- format = 0
+    
+    	self.format = 0
+    	
+    	for x,y,z,r,g,b in string.gmatch(data, '(%-*%d+%p*%d*) (%-*%d+%p*%d*) (%-*%d+%p*%d*) (%d+) (%d+) (%d+)\n') do 
+    	
+    		x = tonumber(x)
+	    	y = tonumber(y)
+    		z = tonumber(z)
+    		r = tonumber(r)
+	    	g = tonumber(g)
+    		b = tonumber(b)
+		    
+    		if math.sqrt(math.pow(x,2)+math.pow(y,2)) < rad2d then
+	  			count = count + 1
+	      	  	meanz = meanz + z
+				table.insert(xyz_table,{x,y,z})
+				table.insert(rgb_table,{r,g,b})
+			end
+      	end
+    end
+    
+    collectgarbage()
+	
+	if count == 0 then
+    	print('no points found!')
+    	return
+    end
+        
+    self.points = torch.Tensor(xyz_table)
+	self.count = count;
+	self.centroid = torch.Tensor({{meanx, meany, meanz}})/(count+0.000001)
+	local stdrd = math.sqrt((self.points-self.centroid:repeatTensor(self.count,1)):pow(2):sum(2):mean())
+	local perc = 0.75
+	
+	print("radius: "..radius..", stdrd: "..(stdrd*numstd))
+	
+	if (perc*radius) > (stdrd * numstd) then
+	
+		radius = stdrd * numstd
+		print("make second pass with new radius: "..radius)
+		
+		count = 0
+		hw_table={}
+		xyz_table={}
+		rgb_table={}
+		meanx = 0
+		meany = 0
+	  	meanz = 0
+	  		
+		if self.format == 1 then
+			for w,h,x,y,z,r,g,b in string.gmatch(data, '(%d+) (%d+) (%-*%d+%p*%d*) (%-*%d+%p*%d*) (%-*%d+%p*%d*) (%d+) (%d+) (%d+)\n') do 
+    
+ 			   	w = tonumber(w)
+		    	h = tonumber(h)
+    			x = tonumber(x)
+		    	y = tonumber(y)
+    			z = tonumber(z)
+		    	r = tonumber(r)
+    			g = tonumber(g)
+		    	b = tonumber(b)
+    	
+    			h=h+1
+				w=w+1
+		
+				if self.centroid[1]:dist(torch.Tensor({x,y,z})) < radius then
+			  		count = count + 1
+      			  	meanz = meanz + z
+					table.insert(xyz_table,{x,y,z})
+					table.insert(rgb_table,{r,g,b})
+		    	  	if h > self.height then
+	    		    	self.height = h;
+				    end
+				 	if w > self.width then
+        				self.width = w;
+			  		end
+      				table.insert(hw_table,{h,w,count})
+		      	end
+		    end
+		elseif self.format == 0 then
+			for x,y,z,r,g,b in string.gmatch(data, '(%-*%d+%p*%d*) (%-*%d+%p*%d*) (%-*%d+%p*%d*) (%d+) (%d+) (%d+)\n') do 
+    			x = tonumber(x)
+	    		y = tonumber(y)
+	    		z = tonumber(z)
+    			r = tonumber(r)
+	    		g = tonumber(g)
+    			b = tonumber(b)
+	    		if math.sqrt(math.pow(x,2)+math.pow(y,2)) < rad2d then
+		  			count = count + 1
+	    	  	  	meanz = meanz + z
+					table.insert(xyz_table,{x,y,z})
+					table.insert(rgb_table,{r,g,b})
+				end
+    	  	end
+		end
+		
+		collectgarbage()
+		
+		self.points = torch.Tensor(xyz_table)
+		self.count = count;
+		self.centroid = torch.Tensor({{meanx, meany, meanz}})/(count+0.000001)
+		
+	end
+	
+	self.rgb = torch.Tensor(rgb_table)
+	if self.format == 1 then
+	    self.index = torch.zeros(self.height, self.width);
+	    for i=1,#hw_table do
+	    	local h = hw_table[i][1]
+	    	local w = hw_table[i][2]
+	    	local c = hw_table[i][3]
+	    	if h==0 or w == 0 or h>self.index:size(1) or w>self.index:size(2) then
+	    		print('i: '..i..', h: '..h..', w: '..w..', c: '..c)
+	    		print(self.index:size())
+	    	end
+	    	self.index[h][w]=c
+	    end
+	end
+	
+	self:reset_point_stats()
+    print("count: "..self.count..", height: "..self.height..", width: "..self.width);
+    
 end
 
 function PointCloud:set_pc_file(pcfilename, radius, numstd)
-	if pcfilename and util.fs.is_file(pcfilename) and util.fs.extname(pcfilename)==PC_FILE_EXTENSION then
+	if pcfilename and util.fs.is_file(pcfilename) 
+				  and util.fs.extname(pcfilename)==PC_ASCII_EXTENSION then
     
     	local file = io.open(pcfilename, 'r');
+    	
 	    local count = 0;
     	self.height = 0;
 	    self.width = 0;
@@ -209,10 +466,30 @@ function PointCloud:set_pc_file(pcfilename, radius, numstd)
   	collectgarbage();
 end
 
-function PointCloud:write(filename)
+function PointCloud:write(filename, option)
+	local pts = self.points:clone()
 	local file = io.open(filename, 'w');
-	for i=1,self.count do
-		file:write(''..self.points[i][1]..' '..self.points[i][2]..' '..self.points[i][3]..' '..self.rgb[i][1]..' '..self.rgb[i][2]..' '..self.rgb[i][3]..'\n')
+	if option and option == 'b' then
+		file.close()
+		file = io.open(filename, 'wb');
+		local minx = math.floor(self.minval[1][1] * 1000)
+		local miny = math.floor(self.minval[1][2] * 1000)
+		local minz = math.floor(self.minval[1][3] * 1000)
+		file:write(''..string.format('%X',minx)..' '..string.format('%X',miny)..' '..string.format('%X',minz)..' 0 0 0\n')
+		pts = (pts*1000):floor()
+		for i = 1,self.count do
+			local x = string.format('%X',(pts[i][1]-minx))
+			local y = string.format('%X',(pts[i][2]-miny))
+			local z = string.format('%X',(pts[i][3]-minz))
+			local r = string.format('%X',self.rgb[i][1])
+			local g = string.format('%X',self.rgb[i][2])
+			local b = string.format('%X',self.rgb[i][3])
+			file:write(''..x..' '..y..' '..z..' '..r..' '..g..' '..b..'\n')
+		end
+	else
+		for i=1,self.count do
+			file:write(''..pts[i][1]..' '..pts[i][2]..' '..pts[i][3]..' '..self.rgb[i][1]..' '..self.rgb[i][2]..' '..self.rgb[i][3]..'\n')
+		end
 	end
 	file:close()
 end
