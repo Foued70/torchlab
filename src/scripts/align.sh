52f4e43797b3951ec72aeff29aa1cd4fe3f9bb66
Marco Scoffier <marco@floored.com>
2013-08-13 14:07:23 -0400

    switch to enfuse for Louvre images.

--- src/scripts/align.sh
+++ src/scripts/align.sh
@@ -1,10 +1,15 @@
 basename=chp_
-for i in `seq -w $1 3 $2` 
+for i in `seq $1 3 $2` 
 do 
-one=`ls ${basename}*tiff | grep ${basename}'0*'${i}.tiff`
-two=`ls ${basename}*tiff | grep ${basename}'0*'$((i + 1)).tiff` 
-thr=`ls ${basename}*tiff | grep ${basename}'0*'$((i + 2)).tiff`
-outname=${one%%.tiff}_${two%%.tiff}_${thr%%.tiff}.hdr
-echo $outname 
-align_image_stack -v -o ${outname} ${one} ${two} ${thr}
+one=`ls ${basename}*tiff | grep ${basename}'00*'${i}'.tiff'`
+two=`ls ${basename}*tiff | grep ${basename}'00*'$((i + 1))'.tiff'` 
+thr=`ls ${basename}*tiff | grep ${basename}'00*'$((i + 2))'.tiff'`
+outname=${one%%.tiff}_${two%%.tiff}_${thr%%.tiff}.tiff
+echo combining ${one} ${two} ${thr} $outname 
+# align_image_stack creates an HDR in radiance .hdr format only.
+# align_image_stack -v -o ${outname} ${one} ${two} ${thr}
+
+# enfuse is for creating a LDR image which looks perceptually good.
+# More pleasing than tone mapping in many respects
+enfuse -d 8 -o ${outname} ${one} ${two} ${thr}
 done
