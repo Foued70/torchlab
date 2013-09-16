3ea3004a1852b142fb2c35bcc9d81aa6ef2e2198
Marco Scoffier <marco@floored.com>
2013-09-16 15:13:46 -0400

    move local to global pose and rotation to PointCloud.  example use in octomap/voxel_tree_from_multiple_pointclouds.lua

--- src/octomap/voxel_tree_from_multiple_pointclouds.lua
+++ src/octomap/voxel_tree_from_multiple_pointclouds.lua
@@ -24,6 +24,7 @@ end
 
 _G.pc1 = pc1
 _G.pc2 = pc2
+
 -- rotation matrix from Stav's output (put 296 in 297 coordinates
 _G.rot = torch.Tensor({{0.9058269262,-0.4292269647,0,-1.978203177},
                        {0.4292269647,0.9058269262,0,1.413987041,},
@@ -32,32 +33,39 @@ _G.rot = torch.Tensor({{0.9058269262,-0.4292269647,0,-1.978203177},
 
 rotq  = geom.quaternion.from_rotation_matrix(rot)
 trans = rot[{{1,3},4}]:clone()
+trans[3] = -pc1.minval[3]
 
-pc1.points = geom.quaternion.rotate_translate(rotq,trans,pc1.points)
+pc1:set_local_to_global_pose(trans)
+pc1:set_local_to_global_rot(rotq)
+pc1:set_local_scan_center(pc1:estimate_faro_pose())
 
--- get z's close.  subtract min value.
-pc1.points[{{},3}]:add(-1*pc1.minval[{1,3}])
+pc1_points     = pc1:get_global_points()
+pc1_pose       = pc1:get_global_scan_center()
+pc1_max_radius = pc1:get_max_radius()
 
-pc1_pose = pc1:estimate_faro_pose()
-pc1_pose:add(trans)
-pc1_max_radius = torch.max(pc1.radius)
 
 log.trace("building tree ...")log.tic()
 _G.t = octomap.Tree.new()
 log.trace(" - in ".. log.toc())
 
 log.trace(" - add points 1 ...")log.tic()
-t:add_points(pc1.points,pc1_pose,pc1_max_radius)
+t:add_points(pc1_points,pc1_pose,pc1_max_radius)
 log.trace(" - in ".. log.toc())
 
 t:stats()
 
-pc2.points[{{},3}]:add(-1*pc2.minval[{1,3}])
-pc2_pose = pc2:estimate_faro_pose()
-pc2_max_radius = torch.max(pc2.radius)
+
+pc2_trans = torch.Tensor({0,0,-pc2.minval[3]})
+
+pc2:set_local_to_global_pose(pc2_trans)
+pc2:set_local_scan_center(pc2:estimate_faro_pose())
+
+pc2_points     = pc2:get_global_points()
+pc2_pose       = pc2:get_global_scan_center()
+pc2_max_radius = pc2:get_max_radius()
 
 log.trace(" - add points 2 ...")log.tic()
-t:add_points(pc2.points,pc2_pose,pc2_max_radius)
+t:add_points(pc2_points,pc2_pose,pc2_max_radius)
 log.trace(" - in ".. log.toc())
 
 t:stats()
