0bb827c68e36a70d6493c7694b559b856717a075
Marco Scoffier <marco@floored.com>
2013-09-20 22:23:02 -0400

    tweaks to img processing scripts for po_scan

--- src/scripts/rawhdr_enblend.sh
+++ src/scripts/rawhdr_enblend.sh
@@ -1,18 +1,25 @@
+#!/bin/bash
+dcraw=${CLOUDLAB_INSTALL_ROOT}/bin/dcraw.sh
 i=1
 fls=
 outname=
-for f in $* 
+for f in $*
 do
-echo $f ; 
-~/proj/cloudlab/src/scripts/dcraw.sh $f ; 
-fls="$fls ${f%%NEF}tiff"
-outname="$outname`basename ${f%%.NEF}`"
-if [ $((i % 3)) == 0 ]
-then
-enfuse -d 8 -o ${outname}.tiff $fls
-rm $fls
-fls=
-outname=
-fi
-i=$((i+1))
+    echo $f ;
+    ${dcraw} $f ;
+    fls="$fls ${f%%nef}tiff"
+    if [ $outname ]
+    then
+        outname="$outname-`basename ${f%%.nef}`"
+    else
+        outname=`basename ${f%%.nef}`
+    fi
+    if [ $((i % 3)) == 0 ]
+    then
+        enfuse -d 8 -o ${outname}.tiff $fls
+        rm $fls
+        fls=
+        outname=
+    fi
+    i=$((i+1))
 done
