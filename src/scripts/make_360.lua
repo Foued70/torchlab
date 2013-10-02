46e74382a8cf71355141b7c37d36c40b7b231a43
Marco Scoffier <marco@floored.com>
2013-10-02 14:09:30 -0400

    update make_360 to include export to enblend blending

--- src/scripts/make_360.lua
+++ src/scripts/make_360.lua
@@ -1,6 +1,6 @@
 -- Class()
-
-dofile 'util.lua'
+os  = require 'os'
+blend = projection.util.blend 
 
 pi = math.pi
 pi2 = pi * 0.5
@@ -12,7 +12,10 @@ cmd:text('Compute image projections')
 cmd:text()
 cmd:text('Options')
 cmd:option('-imagesdir', 'images/', 'directory with the images to load')
-cmd:option('-imageext', 'JPG', 'filename extention of images to load')
+cmd:option('-fileglob', '', 'string to match images')
+cmd:option('-scale', 1, 'how much to down scale original image')
+cmd:option('-enblend', false, 'use enblend to make final image')
+cmd:option('-outimage', 'output_360.png', 'filename of image output')
 cmd:text()
 
 -- parse input params
@@ -23,40 +26,43 @@ if not util.fs.is_dir(images_dir) then
    error(string.format("Can't find directory: %s", images_dir))
 end
 
-image_ext  = params.imageext
-
-images = util.fs.glob(images_dir,image_ext) 
+fileglob = params.fileglob
+if fileglob == '' then 
+   fileglob = {"tiff","png","jpg","JPG"}
+end
+images = util.fs.glob(images_dir,fileglob) 
 
 if #images == 0 then 
-   error(string.format("No images with ext %s found", image_ext))
+   error(string.format("No images with ext %s found", fileglob))
 end
 
-img = image.load(images[1])
+image_wand   = image.Wand.new(images[1])
+height,width = image_wand:size()
+if scale ~= 1 then 
+   height = math.floor(height * params.scale)
+   width = math.floor(width * params.scale)
+   image_wand:size(width,height)
+end
 
-width  = img:size(3)
-height = img:size(2)
 
 -- images are vertical
 vfov = (97/180) * pi 
 hfov = (74.22/180) * pi
 
 -- output full equirectangular
-out_width  = 2048
-out_height = 1024
 out_hfov   = 2 * pi
-out_vfov   = pi
+out_vfov   = vfov
+out_height = height 
+out_width  = out_height * out_hfov / out_vfov
 
 proj_from = projection.GnomonicProjection.new(width,height,hfov,vfov)
 proj_to   = projection.SphericalProjection.new(out_width,out_height,out_hfov,out_vfov)
 
-p("Testing Image Projection")
-
 log.tic()
 
-indices     = {}
-masks       = {}
-out_images  = {}
-
+_G.masks       = {}
+_G.out_images  = {}
+_G.out_fnames  = ""
 rect_to_sphere = projection.Remap.new(proj_from,proj_to)
 
 time = log.toc()
@@ -69,18 +75,28 @@ delta  = 2 * pi / #images
 
 for i = 1,#images do 
 
-   img    = image.load(images[i])
-
+   image_wand:load(images[i])
+   image_wand:size(width,height)
+   img = image_wand:toTensor('byte',"RGBA","DHW")
    proj_from:set_lambda_phi(lambda,phi)
-   index1D,stride,mask = rect_to_sphere:get_index_and_mask(force)
+   index1D,stride,mask = rect_to_sphere:get_offset_and_mask(force)
    img_out = rect_to_sphere:remap(img)
-
-   table.insert(indices,index1D)
-   table.insert(masks,mask)
-   table.insert(out_images,img_out)
-
+   img_out[4]:copy(mask:eq(0):mul(255))
    time = log.toc()
    printf(" - reproject %2.4fs", time)
+   log.tic()
+   if not params.enblend then 
+      table.insert(masks,mask)
+      table.insert(out_images,img_out)
+   else
+      outname = string.format("tmp_%02d.png", i)
+      image_wand:fromTensor(img_out,"RGBA","DHW")
+      printf(" - saving %s %s",outname, image_wand:imagetype())
+      image_wand:save(outname)
+      out_fnames = out_fnames .. " " .. outname
+   end
+   time = log.toc()
+   printf(" - save in %2.4fs", time)
    log.tic()
 
    lambda = lambda + delta
@@ -90,6 +106,14 @@ end
 
 -- blend
 
-allimg = blend(out_images, masks)
-
-image.display(allimg)
+if (not params.enblend) then 
+   allimg = blend(out_images, masks)
+   
+   image.display(allimg)
+   
+   printf("saving %s", params.outimage)
+   image.save(params.outimage, allimg)
+else 
+   os.execute(string.format("enblend %s -o %s", out_fnames, params.outimage))
+   os.execute(string.format("rm %s", out_fnames))
+end
