ba566487a8860525f1bb2566bbdbd3813ac3151a
Marco Scoffier <marco@floored.com>
2013-10-03 12:30:17 -0400

    first pass generic convex optimization function

--- src/sandbox/marco/align_images.lua
+++ src/sandbox/marco/align_images.lua
@@ -29,7 +29,6 @@ if not util.fs.is_dir(imagesdir) then
    error("Must set a valid path to directory of images to process default -imagesdir images/")
 end
 
-
 -- images are vertical
 vfov = (97/180) * pi
 hfov = (74.8/180) * pi
@@ -65,20 +64,19 @@ p("Aligning images")
 
 log.tic()
 
-mindist       = torch.Tensor(#image_files);
+mindist            = torch.Tensor(#image_files);
 mindist:fill(math.huge);
 precalc            = 2*pi/#image_files
 lambda_wiggle_base = hfov / 4 -- in radians (portion of precalc)
 phi_wiggle_base    = vfov / 10 -- in radians (portion of precalc)
-maxquant           = 2; -- steps of wiggle from -maxquant to maxquant
 
+-- setup best first guess
 _G.best_delta = torch.Tensor(2,#image_files)
 best_delta[1]:fill(precalc)
 best_delta[2]:fill(0)
 
-best_distance = torch.Tensor(#image_files);
-best_area     = torch.Tensor(#image_files);
-best_quant    = torch.Tensor(#image_files);
+_G.best_scores = torch.Tensor(#image_files);
+
 
 proj_from     = projection.GnomonicProjection.new(width,height,hfov,vfov)
 
@@ -91,167 +89,251 @@ proj_to       = projection.SphericalProjection.new(canvas_width,canvas_height,ca
 
 rect_to_sphere = projection.Remap.new(proj_from,proj_to)
 
-done_lambda = {}
--- function find_best_lambda(image_files,best_delta
--- find horizontal offsets
--- go through all images find lambda
-for i = 1,#image_files do
-   collectgarbage()
+-- optimize <func()> with respect to <start_param>, and current best
+-- result <best_result> by testing +,- <wiggle> and repeating binary
+-- search by halving wiggle until wiggle is smaller that <stop>.
+-- Arguments can be passed to <func()> with the args in {...}
+function optimize(wiggle,stop,start_param,best_result,func,...)
+   print("Optimizing")
+   count = 0
+   current_best_result = best_result
+   best_param          = start_param
+   while (wiggle >= stop) do 
+      log.tic()
+      for _,wig in pairs({ -wiggle,wiggle}) do
+         param = best_param + wig
+         test_val = func(param,...)
+         if test_val < current_best_result then
+            best_param          = param
+            current_best_result = test_val;
+            printf("new best found %2.4f %2.4f", best_param, current_best_result);
+         end
+      end
+      printf(" - [%d] wiggle %2.4f tested in %2.2fs param: %2.4f, score: %2.4f",
+             count,wiggle,log.toc()/1000, best_param, current_best_result)
+      wiggle = wiggle / 2
+      count = count + 1
+   end
+   return best_param, current_best_result
+end
 
-   -- left image is at negative the simple pie split.
-   proj_from:set_lambda_phi(-precalc,0)
-   index1D1,stride1,mask1 = rect_to_sphere:get_offset_and_mask(force)
-   mm1 = mask1:eq(0);
+function compute_lambda_score(lambda,phi,image_right,mask_left,projected_image_left)
+   proj_from:set_lambda_phi(lambda,phi)
+   _,_,mask_right = rect_to_sphere:get_offset_and_mask(force)
+            
+   mask_right = mask_right:eq(0)
+   overlap_mask = mask_right:cmul(mask_left)
+            
+   area = overlap_mask:sum();
+            
+   projected_image_right = rect_to_sphere:remap(image_right)
+            
+   image_diff = projected_image_left - projected_image_right
+            
+   image_dist = image_diff:abs():sum(1):squeeze():cmul(overlap_mask:float())
+            
+   return image_dist:sum()/area;
+end
 
-   _G.img_out_l = rect_to_sphere:remap(img_l)
-
-   -- load right image and set size
-   image_wand:load(image_files[i])
+function find_best_lambda(image_files,best_delta,wiggle,stop)
+   -- find horizontal offsets
+   -- go through all images find lambda
+   previ = #image_files
+   image_wand:load(image_files[previ])
    image_wand:size(width,height)
-   img_r = image_wand:toTensor('float',"RGB","DHW")
-
-   lambda_anchor = 0
-   phi_anchor    = 0
-   lambda_wiggle = lambda_wiggle_base
-   done_lambda = {}
-   while (lambda_wiggle >= rad_per_pixel) do
-
-      best_lambda = lambda_anchor
-      count       = 1
-      bestsum     = 0
-      bestdiff    = 0
-
-      -- do horizontal
-      for dd = -maxquant,maxquant do
-         collectgarbage()
-         lambda = lambda_anchor + lambda_wiggle * (dd/maxquant);
-         if (done_lambda[lambda]) then
-            printf(" - skipping lambda: %f = ss %f",lambda,done_lambda[lambda])
-         else
-            printf(" - testing lambda: %f", lambda)
-            
-            proj_from:set_lambda_phi(lambda,phi_anchor)
-            index1D2,stride2,mask2 = rect_to_sphere:get_offset_and_mask(force)
-            
-            mm2 = mask2:eq(0)
-            overlap_mask = mm2:cmul(mm1)
-            
-            area = overlap_mask:sum();
-            
-            _G.img_out_r = rect_to_sphere:remap(img_r)
-            
-            imdiff = img_out_l - img_out_r
-            
-            imdist = imdiff:abs():sum(1):squeeze():cmul(overlap_mask:float())
-            
-            ss = imdist:sum()/area;
-            if ss < mindist[i] then
-               best_lambda      = lambda
-               best_delta[1][i] = precalc + lambda
-               best_distance[i] = ss;
-               mindist[i]       = ss;
-               best_area[i]     = area;
-               best_quant[i]    = dd;
-               printf("new best lambda found for %d at %d: %d %s %s %s", i, count, dd, lambda, ss, area);
-               
-            end
-            done_lambda[lambda] = ss
-            count = count + 1
-         end
-      end
-      -- recenter on best lambda
-      lambda_anchor = best_lambda
-      -- split the lambda_wiggle
-      lambda_wiggle = lambda_wiggle / maxquant
-      -- store for debug
-   end
-   img_l = img_r
-end
-
-allmindist = mindist:sum()
-best_phi = 0
-phi_wiggle = phi_wiggle_base
-done_phi = {}
--- do vertical
-while (phi_wiggle >= rad_per_pixel) do
-   count = 1
-   for dd = -maxquant,maxquant do
+   image_left = image_wand:toTensor('float',"RGB","DHW")
+   
+   -- do optimization for each pair
+   for i = 1,#image_files do
       collectgarbage()
-      lambda = best_delta[1][1]
-      best_phi = phi_anchor
-      phi = phi_anchor + phi_wiggle * (dd/maxquant);
-      if done_phi[phi] then 
+      -- setup left image is at negative current best offset.
+      proj_from:set_lambda_phi(-best_delta[1][i],best_delta[2][previ])
+      _,_,mask_left = rect_to_sphere:get_offset_and_mask(force)
+      mask_left = mask_left:eq(0);
 
-         printf(" - skipping phi: %f ss: %f", phi, done_phi[phi])
-      else
-         printf(" - testing phi: %f", phi)
+      projected_image_left = rect_to_sphere:remap(image_left)
 
-         -- assume that phi will be the same for all images as this
-         -- represents the camera not looking exactly at the plane
-         -- of rotation, but slightly above or below.
-         ss = 0
-         image_wand:load(image_files[1])
-         image_wand:size(width,height)
-         img_l = image_wand:toTensor('float',"RGB","DHW")
-         print("image size:", image_wand:size())
-         
-         
+      -- load right image and set size
+      image_wand:load(image_files[i])
+      image_wand:size(width,height)
+      image_right = image_wand:toTensor('float',"RGB","DHW")
 
-         for i = 2,#image_files do
-            collectgarbage()
-            lambda = precalc - best_delta[1][i]
-            proj_from:set_lambda_phi(-precalc,phi)
-            index1D1,stride1,mask1 = rect_to_sphere:get_offset_and_mask(force)
-            mm1 = mask1:eq(0);
-        
-            _G.img_out_l = rect_to_sphere:remap(img_l)
-            image_wand:load(image_files[i])
-            image_wand:size(width,height)
-            img_r = image_wand:toTensor('float',"RGB","DHW")
-            
-            proj_from:set_lambda_phi(lambda,phi)
-            index1D2,stride2,mask2 = rect_to_sphere:get_offset_and_mask(force)
-            _G.img_out_r = rect_to_sphere:remap(img_r)
-            
-            
-            mm2 = mask2:eq(0)
-            overlap_mask = mm2:cmul(mm1)
-            
-            area = overlap_mask:sum();
-            
-            imdiff = img_out_l - img_out_r
-            
-            imdist = imdiff:abs():sum(1):squeeze():cmul(overlap_mask:float())
-            if area < 0 then
-               print("Warning no overlap this should not happen")
-            else
-               ss = ss + imdist:sum()/area;
-            end
-            img_l = img_r
-            mm1   = mm2
-         end
-         if ss < allmindist then
-            best_phi         = phi
-            allmindist       = ss;
-            printf("new best phi found at %d: %d %s %s", count, dd, phi, ss);
-         end
-         done_phi[phi] = ss
-         count = count + 1
-      end
+      -- difference from current best guess lambda
+      lambda_delta  = 0
+      phi           = best_delta[2][i]
+
+      current_best = 
+         compute_lambda_score(lambda_delta, phi,
+                              image_right,
+                              mask_left,projected_image_left)
+      lambda_delta, score = 
+         optimize(wiggle,stop,
+                  lambda_delta,current_best,
+                  compute_lambda_score,
+                  phi,image_right,mask_left,projected_image_left)
+
+      best_delta[1][i] = best_delta[1][i] + lambda_delta
+      best_scores[i] = score;
+      printf("new best lambda found for %d: %2.4f %2.4f", i, best_delta[1][i], score);
+      
+      image_left = image_right
+      previ      = i
    end
-   -- recenter on best lambda
-   phi_anchor = best_phi
-   -- split the lambda_wiggle
-   phi_wiggle = phi_wiggle / maxquant
 end
 
-best_delta[2]:fill(best_phi)
+function compute_phi_score(current_phi, image_files, best_delta)
 
+   -- assume that phi will be the same for all images as this
+   -- represents the camera not looking exactly at the plane
+   -- of rotation, but slightly above or below.
+   score = 0
+   image_wand:load(image_files[#image_files])
+   image_wand:size(width,height)
+   image_left = image_wand:toTensor('float',"RGB","DHW")
+   previ = #image_files
+   for i = 1,#image_files do
+      collectgarbage()
+      lambda_left  = -best_delta[1][i]
+      lambda_right = 0 
+      proj_from:set_lambda_phi(lambda_left,current_phi)
+      _,_,mask_left = rect_to_sphere:get_offset_and_mask(force)
+      mask_left = mask_left:eq(0);
+        
+      projected_image_left = rect_to_sphere:remap(image_left)
+      image_wand:load(image_files[i])
+      image_wand:size(width,height)
+      image_right = image_wand:toTensor('float',"RGB","DHW")
+            
+      proj_from:set_lambda_phi(lambda_right,current_phi)
+      _,_,mask_right = rect_to_sphere:get_offset_and_mask(force)
+      projected_image_right = rect_to_sphere:remap(image_right)
+                        
+      mask_right = mask_right:eq(0)
+      overlap_mask = mask_right:cmul(mask_left)
+      area = overlap_mask:sum()
+
+      image_diff = projected_image_left - projected_image_right
+            
+      image_dist = image_diff:abs():sum(1):squeeze():cmul(overlap_mask:float())
+      if area <= 0 then
+         print("Warning no overlap this should not happen")
+      else
+         score = score + image_dist:sum()/area;
+      end
+      image_left = image_right
+      mask_left  = mask_right
+   end
+   return score
+end
+
+
+function find_best_phi(image_files, best_delta, wiggle, stop, current_phi,current_best_score)
+   wiggle      = wiggle or phi_wiggle_base
+   stop        = stop or rad_per_pixel
+   current_phi = current_phi or 0
+   best_phi    = current_phi 
+   current_best_score = current_best_score or compute_phi_score(current_phi,image_files,best_delta)
+   printf("Adjusting global phi: %f", current_phi)
+   test_phi, score = 
+      optimize(wiggle,stop,
+               best_phi,current_best_score,
+               compute_phi_score,
+               image_files,best_delta)
+   
+   if score < current_best_score then
+      best_phi = test_phi
+      current_best_score = score
+   end
+   best_delta[2]:fill(best_phi)
+   printf("new best phi found %2.4f %2.4f", best_phi, current_best_score);
+   return best_phi, current_best_score
+end
+
+function compute_vfov_score(current_vfov, image_files, best_delta)
+
+   proj_from = 
+      projection.GnomonicProjection.new(width,height,hfov,current_vfov)
+
+   score = 0
+   image_wand:load(image_files[#image_files])
+   image_wand:size(width,height)
+   image_left = image_wand:toTensor('float',"RGB","DHW")
+   previ = #image_files
+   for i = 1,#image_files do
+      collectgarbage()
+      lambda_left  = -best_delta[1][i]
+      phi_left     = best_delta[2][previ]
+      lambda_right = 0 
+      phi_right    = best_delta[2][i]
+
+      proj_from:set_lambda_phi(lambda_left,phi_left)
+      _,_,mask_left = rect_to_sphere:get_offset_and_mask(force)
+      mask_left = mask_left:eq(0);
+        
+      projected_image_left = rect_to_sphere:remap(image_left)
+      image_wand:load(image_files[i])
+      image_wand:size(width,height)
+      image_right = image_wand:toTensor('float',"RGB","DHW")
+            
+      proj_from:set_lambda_phi(lambda_right,phi_right)
+      _,_,mask_right = rect_to_sphere:get_offset_and_mask(force)
+      projected_image_right = rect_to_sphere:remap(image_right)
+                        
+      mask_right   = mask_right:eq(0)
+      overlap_mask = mask_right:cmul(mask_left)
+      area         = overlap_mask:sum()
+
+      image_diff = projected_image_left - projected_image_right
+            
+      image_dist = 
+         image_diff:abs():sum(1):squeeze():cmul(overlap_mask:float())
+      if area <= 0 then
+         print("Warning no overlap this should not happen")
+      else
+         score = score + image_dist:sum()/area;
+      end
+      image_left = image_right
+      mask_left  = mask_right
+   end
+   return score
+end
+
+function find_best_vfov(image_files, best_delta, wiggle, stop, current_vfov,current_best_score)
+   wiggle       = wiggle or phi_wiggle_base
+   stop         = stop or rad_per_pixel
+   current_vfov = current_vfov or vfov
+   best_vfov    = current_vfov
+   current_best_score = current_best_score or 
+      compute_vfov_score(current_vfov,image_files,best_delta)
+   printf("Adjusting vfov: %f",current_vfov)
+   test_vfov, score = 
+      optimize(wiggle,stop,
+               current_vfov,current_best_score,
+               compute_vfov_score,
+               image_files,best_delta)
+   
+   if score < current_best_score then
+      best_vfov = test_vfov
+      current_best_score = score
+   end
+   
+   printf("new best vfov found %2.4f %2.4f", best_vfov, current_best_score);
+   return best_vfov, current_best_score
+end
+   
+find_best_lambda(image_files, best_delta, lambda_wiggle_base, rad_per_pixel)
+best_score = best_scores:sum()
+print(best_scores)
+printf("current best_score: %f", best_score)
+best_phi, best_score = find_best_phi(image_files, best_delta, phi_wiggle_base, rad_per_pixel, 0, best_score)
+printf("current best_score: %f", best_score)
+best_vfov, best_score = find_best_vfov(image_files, best_delta, vfov*0.05, rad_per_pixel, vfov, best_score)
+printf("current best_score: %f", best_score)
+
+-- display
 _G.mapped_image_files = {}
 _G.masks = {}
 _G.out_fnames = ""
-
--- display
 
 -- output full equirectangular
 out_hfov   = 2 * pi + hfov
@@ -259,6 +341,7 @@ out_vfov   = vfov + 0.1 * vfov
 out_height = height
 out_width  = out_height * out_hfov / out_vfov
 
+proj_from = projection.GnomonicProjection.new(width,height,hfov,best_vfov)
 proj_to   = projection.SphericalProjection.new(out_width,out_height,out_hfov,out_vfov)
 
 rect_to_sphere = projection.Remap.new(proj_from,proj_to)
@@ -305,6 +388,6 @@ printf("saving %s", params.outimage)
 image.save(params.outimage, allimg)
 
 if (params.enblend) then
-   os.execute(string.format("enblend %s -o %s", out_fnames, params.outimage))
+   os.execute(string.format("enblend %s -o enblend_%s", out_fnames, params.outimage))
    os.execute(string.format("rm %s", out_fnames))
 end
